6ffa8118576b130e811be9e35aa097fb587f5e2f
Femonzor <wayneyang3@gmail.com>
2018-10-29 23:17:07 +0800

    react native upgrade

--- android/app/src/main/java/com/react-native-pet/MainActivity.java
+++ android/app/src/main/java/com/react-native-pet/MainActivity.java
@@ -1,4 +1,4 @@
-package com.reactnativepet;
+package com.react-native-pet;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "ReactNativePet";
+        return "react-native-pet";
     }
 }
--- ios/react-native-petTests/react-native-petTests.m
+++ ios/react-native-petTests/react-native-petTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface ReactNativePetTests : XCTestCase
+@interface react-native-petTests : XCTestCase
 
 @end
 
-@implementation ReactNativePetTests
+@implementation react-native-petTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
