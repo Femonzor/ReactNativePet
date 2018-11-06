5d362d636057f0c1d6d11108fecd2e5b6c3e932d
Femonzor <wayneyang3@gmail.com>
2018-11-06 23:06:10 +0800

    rename project

--- android/app/src/main/java/com/reactnativepet/MainActivity.java
+++ android/app/src/main/java/com/reactnativepet/MainActivity.java
@@ -1,4 +1,4 @@
-package com.react-native-pet;
+package com.reactnativepet;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "react-native-pet";
+        return "ReactNativePet";
     }
 }
--- android/app/src/main/java/com/reactnativepet/MainApplication.java
+++ android/app/src/main/java/com/reactnativepet/MainApplication.java
@@ -1,11 +1,11 @@
-package com.react-native-pet;
+package com.reactnativepet;
 
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.imagepicker.ImagePickerPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.imagepicker.ImagePickerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -26,9 +26,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new ImagePickerPackage(),
             new ReactVideoPackage(),
-            new VectorIconsPackage()
+            new VectorIconsPackage(),
+            new ImagePickerPackage()
       );
     }
 
